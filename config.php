66fbfb77ccc34f0513e6bf6a00ca9c7b3d3e77ba
ericchang60917 <ericchang60917@gmail.com>
2020-07-26 16:18:56 +0800

    新增config檔

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+//Line Developers Channel Secret and Channel Access Token
+$ChannelSecret = '你的Channel secret'; 
+$ChannelAccessToken = '你的Channel access token'; 
+ 
+ 
+//DB連線資訊
+$db_config=[
+    'dbHost'=>'127.0.0.1',
+    'dbPort'=>'3306',
+    'dbName'=>'',
+    'dbUser'=>'',
+    'dbPass'=>'',
+    'charset'=>'utf-8',
+];
+
+
+
+?>
--- index.php
+++ index.php
@@ -2,22 +2,10 @@
 
 
 include('class_pdo.php');
+include('config.php');
+
 $action=isset($_GET['action']) &&trim($_GET['action']) ? $_GET['action'] : '' ;
 
-//Line Developers Channel Secret and Channel Access Token
-$ChannelSecret = '你的Channel secret'; 
-$ChannelAccessToken = '你的Channel access token'; 
- 
- 
-//DB連線資訊
-$db_config=[
-    'dbHost'=>'127.0.0.1',
-    'dbPort'=>'3306',
-    'dbName'=>'',
-    'dbUser'=>'',
-    'dbPass'=>'',
-    'charset'=>'utf-8',
-];
 
 
 function PostJsonCurl($ChannelAccessToken,$url,$postData){
